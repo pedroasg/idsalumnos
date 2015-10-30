6a6de7b4371ff8cbe20b5548b15acc5e0ed5e302
Pedro Sordo <pedro@bitlab.mx>
2015-10-29 20:05:02 -0500

    ssl remove from iframe

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,7 @@ module Idscomitee
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
     config.assets.precompile += Ckeditor.assets
     config.assets.precompile += %w(ckeditor/*)
+    config.middleware.use Rack::SslEnforcer, :except => [ \.*\/frame$/ ],
+                                             :strict => true
   end
 end
