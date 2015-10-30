7454460d1691a43a341f8d5bc57b5a8519d132e8
Pedro Sordo <pedro@bitlab.mx>
2015-10-29 20:08:19 -0500

    watevs

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,4 +1,5 @@
 class TournamentsController < ApplicationController
+   force_ssl except: [:watch]
   def index
     @tournaments = Tournament.all.order(created_at: :desc).paginate(:page => params[:page], :per_page => 10)
   end
--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,5 @@ module Idscomitee
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
     config.assets.precompile += Ckeditor.assets
     config.assets.precompile += %w(ckeditor/*)
-    config.middleware.use Rack::SslEnforcer, :except => [ \.*\/frame$/ ],
-                                             :strict => true
   end
 end
