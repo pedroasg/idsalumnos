469c5fa3e1f42d96f501509a05afb95aef9a6fcb
Pedro Sordo <pedro@bitlab.mx>
2015-10-30 13:40:55 -0500

    new stuff

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,5 +1,4 @@
 class TournamentsController < ApplicationController
-   force_ssl except: [:watch]
   def index
     @tournaments = Tournament.all.order(created_at: :desc).paginate(:page => params[:page], :per_page => 10)
   end
