126d84c208040e111df4e8b4c0ab7db0cde23a02
Pedro Sordo <pedro@bitlab.mx>
2015-11-18 12:26:37 -0600

    course js

--- app/assets/javascripts/courses.js
+++ app/assets/javascripts/courses.js
@@ -1,8 +1,18 @@
 (function ($) {
 
-    function statusColor() {
-      ($("#status").text("Inscripciones abiertas").css("color", "green"));
-    }
+  function statusColor() {
+    var course = $(this).find("course"),
+        status = $(".course").find("#status");
+
+    status.each(function(_, elm){
+    console.log($(elm).text() == "Inscripciones abiertas")
+      if ($(elm).text() == "Inscripciones abiertas") {
+        $(elm).css("color", "green");
+      } else {
+        $(elm).css("color", "red");
+      }
+    })
+  }
 
   $(document).ready(function() {
     statusColor();
